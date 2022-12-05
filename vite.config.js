b50e1574c16e0651845a12c8207246a692c714ec
arnoson <omasocke@hotmail.de>
2022-12-05 17:33:32 +0100

    chore: check for template index.js

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,29 @@
-import { resolve } from 'path'
-import fs from 'fs'
-import kirby from 'vite-plugin-kirby'
+import { resolve } from "path";
+import { readdirSync, existsSync } from "fs";
+import kirby from "vite-plugin-kirby";
 
-const root = 'src'
-const templateDir = resolve(__dirname, `${root}/templates`)
-const templates = fs
-  .readdirSync(templateDir)
-  .filter(file => !(/^\./).test(file))
-  .filter(file => fs.statSync(`${templateDir}/${file}`).isDirectory())
+const templates = readdirSync("src/templates")
+  .filter((name) => !/^\./.test(name))
+  .filter((name) => existsSync(`src/templates/${name}/index.js`));
 
 const input = Object.fromEntries([
-  ...templates.map(template => [
-    template,
-   `${templateDir}/${template}/index.js`
-  ]),
-  ['shared', resolve(__dirname, `${root}/index.js`)] 
-])
+  ...templates.map((name) => [name, `src/templates/${name}/index.js`]),
+  ["shared", resolve(__dirname, `${root}/index.js`)],
+]);
 
 export default ({ mode }) => ({
-  root: 'src',
-  base: mode === 'development' ? '/' : '/dist/',
+  root: "src",
+  base: mode === "development" ? "/" : "/dist/",
 
   resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
-  },  
-
-  build: {
-    outDir: resolve(process.cwd(), 'public/dist'),
-    emptyOutDir: true,
-    rollupOptions: { input }
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
   },
 
-  plugins: [kirby()]
-})
+  build: {
+    outDir: "public/dist",
+    emptyOutDir: true,
+    rollupOptions: { input },
+  },
+
+  plugins: [kirby()],
+});
